23db346c8a8b95c49bc4469b42f4845d7f57ddaa
Javid-C <javidia@code.edu.az>
2021-12-15 18:16:14 +0400

    Initial commit

---
+++ FirstWebApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApp.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ActionResult Index()
+        {
+            //ViewResult view = new ViewResult();
+            //view.ViewName = "Index";
+            //return Content("Hello P223");
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact(string firstname,string surname)
+        {
+            string fullname = $"{firstname} {surname}";
+            return Content(fullname);
+            return View();
+        }
+
+        public ActionResult FAQ(int id)
+        {
+            return Content(id.ToString());
+            return View();
+        }
+    }
+}
